17cc13c951c2982530f01c08160b435c7d038eae
quasarjohn <quasarjohn21@gmail.com>
2016-11-19 12:48:03 +0800

    Added filter on enter budget- only numbers and one decimal point are allowed.
    TODO
    
    SelectDateFragment and confirmation fragment before the home screen.

---
+++ app/src/main/java/com/berstek/payso/model/SetupTempData.java
@@ -0,0 +1,70 @@
+package com.berstek.payso.model;
+
+/**
+ * Created by John on 11/19/2016.
+ */
+
+public class SetupTempData {
+
+    private String currency = "", symbol = "",
+                    country = "", cycleType = "",  cycleStart = "";
+
+    private double budget = 0;
+    private int divisor = 0;
+
+    public int getDivisor() {
+        return divisor;
+    }
+
+    public void setDivisor(int divisor) {
+        this.divisor = divisor;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCycleType() {
+        return cycleType;
+    }
+
+    public void setCycleType(String cycleType) {
+        this.cycleType = cycleType;
+    }
+
+    public String getCycleStart() {
+        return cycleStart;
+    }
+
+    public void setCycleStart(String cycleStart) {
+        this.cycleStart = cycleStart;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+}
